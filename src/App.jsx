3e3240dfc0b35b68d94ec72de4447297db33d3df
LuisDani07 <tics270118@gmail.com>
2022-11-07 10:33:23 -0700

    esqueleto inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './Components/NavBar';
+import './styles.css'
+
+function App() {
+  return (
+        <>
+          <NavBar/>
+        </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <header>
+        <a href="#" className='logo'>SaltaRanas</a>
+        <ul className='nav'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+        </ul>
+    </header>
+  )
+}
+
+export default NavBar
