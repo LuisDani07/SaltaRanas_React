eb2fb4275bc49a72d996edb079c7d356c75779a0
LuisDani07 <tics270118@gmail.com>
2022-11-08 10:26:54 -0700

    estructura del sobre nosotros

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,8 +2,13 @@
 
 function About() {
   return (
-    <aside>
-          
+    <aside className="about">
+           <div className="contentBx">
+               <h2>Historia</h2>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero commodi reprehenderit facere maiores corporis exercitationem perspiciatis harum quis velit, similique aspernatur doloremque, amet optio. Eum, ad placeat, tempora necessitatibus eveniet, dolores voluptas a sapiente harum rerum libero reiciendis iusto fugiat officiis qui molestias itaque eaque animi aut vitae incidunt ipsam? Error deleniti maiores ratione magni quos ex a nam excepturi facilis quod, dignissimos nihil eum ab voluptatum necessitatibus blanditiis sapiente perspiciatis! Mollitia, perspiciatis reiciendis. Modi magnam nisi quae neque quas quod dolore, eius officia adipisci debitis iste nostrum asperiores doloremque beatae, mollitia non explicabo. A perferendis totam impedit? Dolor, rem!</p>
+           </div>
+           <a href="#">Leer más</a>
+           <img src="#" alt="Videojuego" />
     </aside>
   )
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,15 +6,15 @@ function NavBar() {
         <a href="#" className='logo'>SaltaRanas</a>
         <ul className='nav'>
             <li><a href="#">Inicio</a></li>
-            <li><a href="#">Sobre Nosotros</a></li>
-            <li><a href="#">Juegos</a></li>
-            <li><a href="#">Torneos</a></li>
+            <li><a href="#">sobre nosotros</a></li>
+            <li><a href="#">historia</a></li>
+            <li><a href="#">como jugar</a></li>
             <li><a href="#">Contacto</a></li>
         </ul>
         <div className='action'>
             <div className="searchBx">
                 <a href="#"><i className='bx bx-search-alt-2' ></i></a>
-                <input type="text" placeholder='search games'/>
+                <input type="text" placeholder='buscar juegos'/>
             </div>
         </div>
         <div className="toggleMenu"></div>
