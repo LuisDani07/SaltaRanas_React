7308839a9dbd33d975be25f52b51aa1b55a7a42f
LuisDani07 <tics270118@gmail.com>
2022-11-08 08:18:57 -0700

    importando icono de buscar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,11 +6,18 @@ function NavBar() {
         <a href="#" className='logo'>SaltaRanas</a>
         <ul className='nav'>
             <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Tournament</a></li>
+            <li><a href="#">Contact</a></li>
         </ul>
+        <div className='action'>
+            <div className="searchBx">
+                <a href="#"><i class='bx bx-search-alt-2' ></i></a>
+                <input type="text" placeholder='search games'/>
+            </div>
+        </div>
+        <div className="toggleMenu"></div>
     </header>
   )
 }
