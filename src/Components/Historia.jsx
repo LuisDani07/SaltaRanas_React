450d47e7c54ee634fa201abe1ba91a92bd490ad6
LuisDani07 <tics270118@gmail.com>
2022-11-21 22:26:25 -0700

    margin top en el banner

--- src/Components/Historia.jsx
+++ src/Components/Historia.jsx
@@ -5,7 +5,17 @@ function Historia() {
       <aside id="historia1" className="historia animeX" >
           <div>
             <h2 >Historia</h2>
-            <p >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium repellat quis cupiditate! Dolor, dolorum, fugit possimus unde architecto sequi amet odit ratione cumque doloremque nihil ullam esse repellat velit sed. Quaerat enim quia atque et similique veniam, error quibusdam placeat nam explicabo tenetur iste nihil natus repellendus fuga quam cum ratione corrupti vero. Quam est voluptatem consequatur ipsum repudiandae. Mollitia quasi facilis modi iusto id nulla quidem. Sapiente cupiditate officia consequuntur sequi esse consectetur dolorem, quod nostrum iusto suscipit temporibus ipsam, minus numquam nam quae nesciunt eaque cum dolores illo commodi! Quas sunt amet est cupiditate in itaque minus eveniet?</p>
+            <p >Fermín una rana que por causas del destino se pierde de su grupo de amigos en un terreno en el cual nunca había estado, por lo tanto todo lo que está experimentando es completamente nuevo para él.
+
+Fermín pertenece al ejército de ranas en el monte rana, por lo tanto no le resulta muy difícil la supervivencia en dichos terrenos, pero en el transcurso de esta travesía se da cuenta de unas frutas “mágicas” las cuales le otorgan una especie de stamina al comerlas lo cual le ayuda a correr más rápido; el decide comer una para probar el efecto que causa en su cuerpo, pero es una sustancia a la cual no está acostumbrado por lo tanto se desmaya al instante. Pasó muchos días sin reaccionar hasta que una especie de lluvia cayó en el bosque y Fermín despertó lo cual le otorgó una especie de poderes los cuales le iban a servir como armas para poder sobrevivir en esos terrenos desconocidos.
+
+Al transcurrir en su camino se topó con un pulpo el cual se hacía llamar “el tentáculos” el cual su poder especial es lanzar un ácido venenoso el cual si te toca te deja bastante dañado por lo tanto te quedas dormido al instante. “El tentáculos” supera considerablemente la fuerza pero Fermín tiene más pasión por vivir lo cual le da un impulso de supervivencia el cual le da el impulso para poder activar su sexto sentido y se hace más veloz; después de una ardua batalla logra vencer a dicho enemigo.
+
+Durante varios largos días fue investigando y recorriendo el terreno en donde estaba, y tras pasar un terreno en el cual había una especie de espinas en el piso gracias a su gran habilidad adquirida en combate. el ambiente se empezó a poner más tenso y la luz del día empezó a bajar y a la distancia vio una sombra que le pareció familiar y Fermín se emocionó porque pensó que era alguno de sus amigos pero para su desgracia era Mushroovil un hongo el cual lanza golpes que afectarán a nuestro personaje ya que si Mushroovil llega a golpearnos nos afectará en nuestros sentidos y nuestra precisión disminuye considerablemente ya que ataca a nuestro campo de visión.
+
+Después de una larga travesía y múltiples peleas logra reencontrarse con sus amigos y entre todos fabrican un vehículo con animales salvajes para poder escapar y volver a casa
+
+</p>
             <img src="#" alt="" />
           </div>
            
