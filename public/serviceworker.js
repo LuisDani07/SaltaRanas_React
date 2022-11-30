05b76196aee921be950110bee1666332eb7d7f36
LuisDani07 <tics270118@gmail.com>
2022-11-30 09:34:02 -0700

    añadiendo elementos al caché

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,45 +1,85 @@
-const CACHE_NAME="version-1";
-const urlsToCache=['index.html'];
+//asignar un nombre y versi´´on al cache
 
+const CACHE_NAME='v1_cache_102';
+//archivos a guardar a cache
+let urlsToCache=[
+   '/',
+   '/1.PNG',
+   '/2.PNG',
+   '/3.PNG',
+   '/4.PNG',
+   '/5.PNG',
+   '/1.PNG',
+   '/128.PNG',
+   '/256.PNG',
+   '/512.PNG',
+   '/Hamburger_svg.png',,
+   '/logo.PNG',
+   '/rana_con_lanza.png',
+   '/tepicGames.PNG',
+   '/videojuego.png',
+   '/manifest.json',
+   'serviceworker.js',
+   'src/styles.css'
+];
 
-const self=this;
-//install sw
-self.addEventListener('install', (event)=>{
-event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then((cache)=>{
-        console.log('open cache');
-        return cache.addAll(urlsToCache);
-    })
-)
-});
-
-//listen for requests
-self.addEventListener('fetch', (event)=>{
-    event.respondWith(
-        caches.match(event.request)
-        .then(async ()=>{
-            try {
-                return await fetch(event.request);
-            } catch {
-                return await caches.match('offline.html');
-            }
+//install
+self.addEventListener('install', e=>{
+    e.waitUntil(    //espera a que se abra el cache
+        caches.open(CACHE_NAME)                        //abrimos  el cache, regresa una promesa
+        .then(cache=>{
+            cache.addAll(urlsToCache)          //regresamos los elementos del arreglo
+            .then(()=>{
+                 self.skipWaiting();  //espera a que see llene el cache
+            })
+        })  
+        .catch(err=>{
+            console.log("no se he registrado el cache", err)
         })
     )
-});
+})
 
-//activate sw
-self.addEventListener('activate', (event)=>{
-    const cacheWhitelist=[];
-    cacheWhitelist.push(CACHE_NAME);
 
-    event.waitUntil(
-        caches.keys().then((cacheNames)=>Promise.all(
-            cacheNames.map((cacheName)=>{
-                if(!cacheWhitelist.includes(cacheName)){
-                    return caches.delete(cacheName)
+
+
+//activate
+
+self.addEventListener('activate', e=>{
+    const cacheWhiteList=[CACHE_NAME]       //guardar todos los elementos que vienen en el cache original
+    e.waitUntil(
+        caches.keys() //el Keys recorre el array
+        .then(cacheNames=>{
+            return Promise.all(
+            //map() nos permite recorrer el array
+            cacheNames.map(cacheName=>{
+                //indesOf es para buscar un elemento del cache
+                //lo siguiente es buscar un elemento y si no se encuentra borrarlo de la cache o si es redundante
+                if(cacheWhiteList.indexOf(cacheName)===-1){
+                    //borrar elementos que no se necesitan
+                    return caches.delete(cacheName);
                 }
             })
-        ))
+        )
+        })
+        .then(()=>{
+            self.clients.claim();
+        })
     )
-});
+})
+
+
+//fetch
+
+self.addEventListener('fetch', e=>{
+    e.respondWith(
+        caches.match(e.request)  //Busca la info en el cache
+        .then(res=>{
+            if(res){
+                        //se se encuentra en el cache devuelve del cache
+                        return res;
+            }
+            return fetch(e.request);
+        })
+    )
+})
+
